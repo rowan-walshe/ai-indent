4c1bfcc9c2eff0b9acb7a2a9f3ecf2c0d514d2f0
Rowan Walshe <rowan@walshe.io>
2023-07-10 23:22:38 +0100

    Fix container build typo

--- .devcontainer/base_image/update_base_image.sh
+++ .devcontainer/base_image/update_base_image.sh
@@ -2,4 +2,4 @@
 
 DOCKER_BUILDKIT=1 docker build -t rowanw/ai-indent-devcontainer:latest -f Dockerfile ../..
 
-docker push rowanw/ai-inde nt-devcontainer:latest
+docker push rowanw/ai-indent-devcontainer:latest
